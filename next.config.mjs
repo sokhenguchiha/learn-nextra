5b5422d77b362444aba23652dc749f2f3706ea9b
sokhenguchiha <sokhengchen@gmail.com>
2024-03-22 10:03:22 -0700

    Initial commit from Create Next App

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import nextra from "nextra";
+
+const nextConfig = {};
+
+const withNextra = nextra({
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.jsx",
+});
+
+export default withNextra(nextConfig);
---
+++ theme.config.jsx
@@ -0,0 +1,8 @@
+const config = {
+	logo: <span>DailyDoc</span>,
+	project: {
+		link: "https://github.com/shuding/nextra",
+	},
+	// ... other theme options
+};
+export default config;
